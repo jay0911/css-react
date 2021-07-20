21c104931a5c6656132da6330960ca932cc7aea0
Joh Ryan Oliveros <johryanoliveros@OETC-MNL-LAP-JohnOliveros.bbrouter>
2021-07-20 20:47:19 +0800

    baseline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route,Redirect } from 'react-router-dom'
+import {createStore,applyMiddleware} from "redux"
+import reducers from './reducers'
+import {Provider} from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import Home from './pages/Home'
+
+const store = createStore(reducers, {},applyMiddleware(ReduxThunk));
 
 function App() {
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        <Component {...props} />
+    )} />
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path='/' exact component={Home} />
+          </Switch>
+        </Router>
+      </Provider>
+    </>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+
+import {combineReducers} from 'redux'
+import TestReducer from './TestReducer'
+
+
+export default combineReducers({
+    test:TestReducer
+})
